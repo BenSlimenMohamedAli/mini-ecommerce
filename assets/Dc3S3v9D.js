7d4bada88b4fe6721d0ba6354a7aa6223d20cb02
BenSlimenMohamedAli <dslimen.15@gmail.com>
2024-03-20 16:33:29 +0100

    Updates

---
+++ assets/Dc3S3v9D.js
@@ -0,0 +1,1 @@
+import{$ as t}from"./BPTXaMip.js";const s=t("navigation",{state:()=>({sidebarOpened:!1,pageTitle:""}),actions:{closeSidebar(){this.$patch(e=>{e.sidebarOpened=!1})},openSidebar(){this.$patch(e=>{e.sidebarOpened=!0})},changePageTitle(e){this.$patch(a=>{a.pageTitle=e})}},getters:{}});export{s as u};
