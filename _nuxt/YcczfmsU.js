9be210c612988719f4658fc09ba820bc54f09372
BenSlimenMohamedAli <dslimen.15@gmail.com>
2024-03-20 16:47:23 +0100

    Updates

--- _nuxt/YcczfmsU.js
+++
@@ -1,1 +0,0 @@
-import{$ as t}from"./B27KVb5O.js";const s=t("navigation",{state:()=>({sidebarOpened:!1,pageTitle:""}),actions:{closeSidebar(){this.$patch(e=>{e.sidebarOpened=!1})},openSidebar(){this.$patch(e=>{e.sidebarOpened=!0})},changePageTitle(e){this.$patch(a=>{a.pageTitle=e})}},getters:{}});export{s as u};
